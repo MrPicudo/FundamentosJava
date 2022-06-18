4d30fd77d684e450ffee336aff312ab56caaef4b
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2022-06-18 17:27:22 -0500

    Creación de getters y setters en mi primer programa de POO

--- src/p20_Clases/Persona.java
+++ src/p20_Clases/Persona.java
@@ -5,6 +5,7 @@ public class Persona {
     String nombre;
     String genero;
     int edad;
+    
     // Después de los atributos, definimos los métodos de nuestra clase.
     public void info() {
         System.out.println("Los atributos de este objeto son: ");
@@ -12,4 +13,23 @@ public class Persona {
         System.out.println(" Género: " + genero);
         System.out.println(" Edad: " + edad + "\n");
     }
+    
+    // Los getters y setters de la clase se pueden crear automáticamente y siempre son sencillos.
+    
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
 }
